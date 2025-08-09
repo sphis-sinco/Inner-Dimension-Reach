25b7f550cad2e8c5ffa161ff7ae082d6dbc15734
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-09 09:13:38 -0500

    ui stuff work + we got hscript too

--- source/play/PlayState.hx
+++ source/play/PlayState.hx
@@ -1,9 +1,20 @@
 package play;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
+
 class PlayState extends FlxState
 {
+	public var ui_box:FlxSprite;
+	public var ui_box_contents:FlxTypedGroup<FlxBasic>;
+
 	override public function create()
 	{
+		ui_box = new FlxSprite().makeGraphic(480, FlxG.height, 0xFFFFFF);
+		ui_box_contents = new FlxTypedGroup<FlxBasic>();
+
+		add(ui_box);
+		add(ui_box_contents);
+
 		super.create();
 	}
 
