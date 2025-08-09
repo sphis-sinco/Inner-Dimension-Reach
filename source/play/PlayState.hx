efee3efc02f06ca42268c3b2a85afbbc75ad3f1c
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-09 09:25:15 -0500

    player and op

--- source/play/PlayState.hx
+++ source/play/PlayState.hx
@@ -1,19 +1,49 @@
 package play;
 
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	public var ui_box:FlxSprite;
 	public var ui_box_contents:FlxTypedGroup<FlxBasic>;
 
+	public var opponent:FlxSprite;
+	public var player:FlxSprite;
+
+	public var addObject = function(object:FlxBasic) {};
+
 	override public function create()
 	{
+		addObject = object ->
+		{
+			if (object == null)
+			{
+				trace('NULL OBJECT, SHOULDNT BE DOIN THAT BRO');
+				return;
+			}
+
+			add(object);
+		}
+
+		opponent = new FlxSprite().makeGraphic(256, 512, FlxColor.GRAY);
+		player = new FlxSprite().makeGraphic(256, 256, FlxColor.PURPLE);
+
+		opponent.y = FlxG.height - opponent.height;
+		player.y = FlxG.height - player.height;
+
+		opponent.screenCenter(X);
+		opponent.x += opponent.width * 1.75;
+		player.screenCenter(X);
+
+		addObject(opponent);
+		addObject(player);
+
 		ui_box = new FlxSprite().makeGraphic(480, FlxG.height);
 		ui_box_contents = new FlxTypedGroup<FlxBasic>();
 
-		add(ui_box);
-		add(ui_box_contents);
+		addObject(ui_box);
+		addObject(ui_box_contents);
 
 		super.create();
 	}
